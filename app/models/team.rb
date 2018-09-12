845a9c28bc718a832255881111e88653286d5556
Kevin Reedy <kevinreedy@gmail.com>
2018-09-11 19:00:56 -0500

    add season to team relation

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
   after_create :set_default_elo
+
   has_and_belongs_to_many :divisions
+  has_many :seasons, through: :divisions
 
   def set_default_elo
     self.elo_cache = 1500
