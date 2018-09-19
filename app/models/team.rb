e65202fd65c48593b80bc487b07e4f11181b3a51
Kevin Reedy <kevinreedy@gmail.com>
2018-09-19 18:10:50 -0500

    add views for divisions

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,13 +4,34 @@ class Team < ApplicationRecord
   has_and_belongs_to_many :divisions
   has_many :seasons, through: :divisions
 
-  def matches
-    Match.where(away_team_id: id).or(Match.where(home_team_id: id))
-  end
-
   def set_default_elo
     self.elo_cache = starting_elo || 1500
     save
   end
 
+  def matches
+    Match.where(away_team_id: id).or(Match.where(home_team_id: id))
+  end
+
+  def record(team_matches = matches)
+    record = { wins: 0, losses: 0 }
+
+    return record unless team_matches.any?
+
+    team_matches.each do |m|
+      if self == m.winner
+        record[:wins] += 1
+      else
+        record[:losses] +=1
+      end
+    end
+
+    record
+  end
+
+  def league_record(division = nil)
+    # If Division specified, return record from this division
+    # Else, return record from _all_ division games
+    division ? record(matches.where(division: division)) : record(matches.where.not(division: nil))
+  end
 end
