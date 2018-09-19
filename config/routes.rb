de3ee05ffb4f655e40fc42cf7e82a7858d776bca
Kevin Reedy <kevinreedy@gmail.com>
2018-09-18 20:20:00 -0500

    add top teams view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'match/new'
-
-  get 'match/create'
-
-  get 'match/index'
+  resources :matches
+  resources :teams, only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
