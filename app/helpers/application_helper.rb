018f21779ad7da4c27a76e23a23c08cee6c0ecd3
Kevin Reedy <kevinreedy@gmail.com>
2018-12-28 16:17:37 -0600

    Refactor rank-board layout to have customizable columns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,37 @@ module ApplicationHelper
     base_title = 'Shuff.io'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def rank_board_width(rank = true, record = true, elo = true, elo_change = true)
+    # default widths
+    width = {
+      rank: 1,
+      name: 6,
+      record: 2,
+      elo: 2,
+      elo_change: 1
+    }
+
+    unless rank
+      width[:rank] = width[:rank] - 1
+      width[:name] = width[:name] + 1
+    end
+
+    unless record
+      width[:record] = width[:record] - 2
+      width[:name] = width[:name] + 2
+    end
+
+    unless elo
+      width[:elo] = width[:elo] - 2
+      width[:name] = width[:name] + 2
+    end
+
+    unless elo_change
+      width[:elo_change] = width[:elo_change] - 1
+      width[:name] = width[:name] + 1
+    end
+
+    width
+  end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -2,4 +2,12 @@ class Season < ApplicationRecord
   has_many :divisions
   has_many :teams, through: :divisions
   has_many :matches, through: :divisions
+
+  def latest?
+    Season.order(:start_date).last == self
+  end
+
+  def started?
+    start_date.past?
+  end
 end
