7a3fa3ff0e6bdb6d7aa02f880a0fe5882a822974
Kevin Reedy <kevinreedy@gmail.com>
2018-12-28 16:26:00 -0600

    Show Active Teams by default instead of All Teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,9 @@
 class TeamsController < ApplicationController
   def index
-    @teams = if params[:current_season]
-               Season.last.teams.order('elo_cache DESC').map { |t| { team: t } }
-             else
+    @teams = if params[:all_teams]
                Team.all.order('elo_cache DESC').map { |t| { team: t } }
+             else
+               Season.last.teams.order('elo_cache DESC').map { |t| { team: t } }
              end
   end
 
