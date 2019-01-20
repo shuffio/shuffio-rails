d4a4bafad44da4cbc9dd6a9a97c3cb1ca0932033
Kevin Reedy <kevinreedy@gmail.com>
2019-01-19 21:18:14 -0600

    Add flash messages to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,14 @@ module ApplicationHelper
 
     width
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then 'alert alert-dismissable alert-primary'
+    when 'success' then 'alert alert-dismissable alert-success'
+    when 'error' then 'alert alert-dismissable alert-danger'
+    when 'alert' then 'alert alert-dismissable alert-warning'
+    else 'alert alert-dismissable'
+    end
+  end
 end
