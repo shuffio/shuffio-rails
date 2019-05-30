bda41067e3d145d23b4ed4f14da925d21eb56819
Kevin Reedy <kevinreedy@gmail.com>
2019-05-29 19:11:21 -0500

    Guard against missing values

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -3,10 +3,12 @@ class LiveController < ApplicationController
     @left_game = Game.find(1)
     @right_game = Game.find(2)
 
-    @team1 = @left_game.yellow_team
-    @team2 = @left_game.black_team
-    @team3 = @right_game.yellow_team
-    @team4 = @right_game.black_team
+    default_team = OpenStruct.new(color: '#ffff00', display_name: 'The Defaults!')
+
+    @team1 = @left_game.yellow_team || default_team
+    @team2 = @left_game.black_team || default_team
+    @team3 = @right_game.yellow_team || default_team
+    @team4 = @right_game.black_team || default_team
     render layout: 'live'
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,13 @@ class Game < ApplicationRecord
   belongs_to :match
   belongs_to :yellow_team, class_name: 'Team', optional: true
   belongs_to :black_team, class_name: 'Team', optional: true
+  after_initialize :default_values
 
   # TODO: game must be 1-3
+
+  private
+
+  def default_values
+    self.frames ||= []
+  end
 end
