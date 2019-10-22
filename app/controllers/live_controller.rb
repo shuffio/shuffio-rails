b09938ab331d8e27e46cd13e4ab65b426ea66199
Trevor Hess <trevor@chef.io>
2019-10-22 15:52:34 -0500

    add shell for lower third view

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -14,4 +14,20 @@ class LiveController < ApplicationController
 
     render layout: 'live'
   end
+
+  def lower_third
+    # TODO: add a view for show(id), but for now only expect one LiveEvent
+    @live_event = LiveEvent.last
+    @left_game = @live_event.left_game
+    @right_game = @live_event.right_game
+
+    default_team = OpenStruct.new(color: '#ffff00', display_name: 'The Defaults!')
+
+    @team1 = @left_game.yellow_team || default_team
+    @team2 = @left_game.black_team || default_team
+    @team3 = @right_game.yellow_team || default_team
+    @team4 = @right_game.black_team || default_team
+
+    render layout: 'live'
+  end
 end
