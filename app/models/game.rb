ed1686b38f719bb14c5453f3db7ebd304694481f
Kevin Reedy <kevinreedy@gmail.com>
2020-03-13 15:27:49 -0500

    Add tests and update Game functionality accordingly

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,13 +3,15 @@ class Game < ApplicationRecord
   belongs_to :yellow_team, class_name: 'Team', optional: true
   belongs_to :black_team, class_name: 'Team', optional: true
 
-  # TODO: validate this enum, and everything else
   enum game_type: { standard_singles: 0, standard_doubles: 1, palms_singles: 2, palms_doubles: 3 }
 
   validate :frames_or_points?
   validate :valid_game_number?
+  validate :points_cannot_tie?
 
   after_initialize :default_values
+
+  # TODO: reject frames with only one score, for example [[0, 0], [8, nil]]
 
   # TODO: re-work views to not need this method, probably in controller?
   def eight_frames
@@ -31,8 +33,9 @@ class Game < ApplicationRecord
     frames.fill(['​', '​'], frames.length, 17 - frames.length).drop(8)
   end
 
-  def completed?
+  def complete?
     return false unless frames # return quickly if frames is nil
+    return false unless frames.size.positive?
 
     if max_frames && max_points
       # frame and point game, whichever comes first
@@ -42,26 +45,36 @@ class Game < ApplicationRecord
       return false if frames.count < max_frames # false if there are frames to go
     elsif max_points
       # point game
+      # TODO: there is some complexity around the game ending when above max points, but still being in a tie that we're not accounting for yet
       return false if (frames.last[0] < max_points) && (frames.last[1] < max_points)
     end
 
     return false if (frames.last[0] == frames.last[1]) && !allow_ties # false if game tied and ties not allowed
 
-    # TODO: this makes sense, right? time to write some tests
-    return false unless max_points && at_game_end_boundary? # false if there are still frames remaining
+    unless max_points
+      return false unless at_game_end_boundary? # false if there are still frames remaining
+    end
 
     true
   end
 
-  def next_hammer
-    frame_count = frames.count + 1
+  def next_frame
+    frames.count + 1
+  end
 
-    case game_type
+  def next_hammer
+    Game.hammer_for_frame(next_frame, game_type)
+  end
+
+  def self.hammer_for_frame(frame_number = 1, type = 'standard_singles')
+    raise 'invalid frame number' unless frame_number.positive?
+
+    case type
     when 'standard_singles'
-      return 'yellow' if frames.count.odd?
-      return 'black' if frames.count.even?
+      return 'yellow' if frame_number.even?
+      return 'black' if frame_number.odd?
     when 'standard_doubles'
-      case frame_count % 4
+      case frame_number % 4
       when 1
         'black'
       when 2
@@ -72,7 +85,7 @@ class Game < ApplicationRecord
         'yellow'
       end
     when 'palms_singles', 'palms_doubles'
-      case frame_count % 4
+      case frame_number % 4
       when 1
         'black'
       when 2
@@ -87,27 +100,8 @@ class Game < ApplicationRecord
     end
   end
 
-  private
-
-  def default_values
-    self.frames ||= []
-  end
-
-  def frames_or_points?
-    return true if max_points
-    return true if max_frames
-
-    false
-  end
-
-  def valid_game_number?
-    return true if number && number.positive?
-
-    false
-  end
-
   def game_end_boundary
-    return 4 if Game.last.standard_doubles?
+    return 4 if standard_doubles?
 
     2
   end
@@ -115,6 +109,26 @@ class Game < ApplicationRecord
   def at_game_end_boundary?
     return false if frames.count.zero?
 
-    (frames.count % Game.last.game_end_boundary).zero?
+    (frames.count % game_end_boundary).zero?
+  end
+
+  private
+
+  def default_values
+    self.frames ||= []
+  end
+
+  def frames_or_points?
+    errors.add(:base, 'must have max_points or max_frames') unless max_points || max_frames
+  end
+
+  def valid_game_number?
+    errors.add(:number, 'must be positive') unless number && number.positive?
+  end
+
+  def points_cannot_tie?
+    return if max_frames
+
+    errors.add(:base, 'point games cannot tie') if max_points && allow_ties
   end
 end
