e553851920c17d79029ba86a9b1fb6caf706c3b6
Trevor Hess <trevor.ghess@gmail.com>
2020-09-16 01:24:13 -0500

    fix team order, add links to teams, truncate long names

--- app/controllers/bracket_controller.rb
+++ app/controllers/bracket_controller.rb
@@ -8,5 +8,9 @@ class BracketController < ApplicationController
 
   def current
     @tournament = Tournament.last
+    @placeholder_team_ids = (685..700).to_a
+    @chicago_final_match = Tournament.last.tournament_groups[0].tournament_rounds.last.matches[0]
+    @brooklyn_final_match = Tournament.last.tournament_groups[1].tournament_rounds.last.matches[0]
+    @chi_bkl_final_match = Tournament.last.tournament_groups[2].tournament_rounds.last.matches[0]
   end
 end
