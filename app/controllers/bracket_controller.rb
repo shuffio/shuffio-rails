6622789593406f7163333b0c2f18e37293f9cebd
Kevin Reedy <kevinreedy@gmail.com>
2020-09-17 14:34:21 -0500

    Add bracket buttons to bracket#current

--- app/controllers/bracket_controller.rb
+++ app/controllers/bracket_controller.rb
@@ -97,6 +97,7 @@ class BracketController < ApplicationController
     @chicago_final_match = Tournament.last.tournament_groups[0].tournament_rounds.last.matches[0]
     @brooklyn_final_match = Tournament.last.tournament_groups[1].tournament_rounds.last.matches[0]
     @chi_bkl_final_match = Tournament.last.tournament_groups[2].tournament_rounds.last.matches[0]
+    @bracket = Bracket.new
   end
 
   private
